378a6162055f8835a05299feb71ba60f312588da
DO VAN HAI <68525744+haidv-2801@users.noreply.github.com>
2020-09-22 21:03:38 +0700

    add dequi file

---
+++ B2/dequi.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(int i) {
+	if(i>=0) {
+		printf("%d ", i); //di thang xuong  10 - 0
+		print(i-1);
+		printf("%d ", i); //di nguoc len 0-10
+	}
+}
+int main()
+{
+	print(10);
+}
+
+
+
+
+
